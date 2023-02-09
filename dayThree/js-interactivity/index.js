57a7809df9c5ad451c714b283075cced10a60529
Parker Sheehan <Parkersheehan007@gmail.com>
2023-02-08 20:37:00 -0600

    day-3 lab Event listeners

---
+++ dayThree/js-interactivity/index.js
@@ -0,0 +1,34 @@
+let message = document.querySelector('#message')
+
+const addMovie = (evt) =>{
+    evt.preventDefault()
+    let inputField = document.querySelector('input').value
+    let movie = document.createElement('li')
+    let movieTitle = document.createElement('span')
+
+
+    movieTitle.textContent = inputField
+    movieTitle.addEventListener('click', crossOffMovie)
+
+    movie.appendChild(movieTitle)
+    let deleteBtn = document.createElement("button")
+    deleteBtn.textContent = ('X')
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+    document.querySelector('ul').appendChild(movie)
+    document.querySelector('input').value = ""
+    message.textContent = "Movie Added!"
+}
+
+const deleteMovie = (evt) => {
+    evt.target.parentNode.remove()
+    message.textContent = "Movie deleted!"
+}
+
+const crossOffMovie = (evt) =>{
+    evt.target.classList.toggle("checked")
+    message.textContent = "Movie checked!"
+}
+
+document.querySelector('form').addEventListener('submit', addMovie)
+
