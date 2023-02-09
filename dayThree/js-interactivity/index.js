8ddfdad974b898b7eb374d59364625b39f8f5fda
Parker Sheehan <Parkersheehan007@gmail.com>
2023-02-09 13:22:19 -0600

    json,serverrequests and more

--- dayThree/js-interactivity/index.js
+++ dayThree/js-interactivity/index.js
@@ -1,34 +1,43 @@
-let message = document.querySelector('#message')
 
-const addMovie = (evt) =>{
+// const addMovie = (evt) =>{
+//     evt.preventDefault()
+//     let inputField = document.querySelector('input').value
+//     let movie = document.createElement('li')
+//     let movieTitle = document.createElement('span')
+
+//     movieTitle.textContent = inputField
+//     movieTitle.addEventListener('click', crossOffMovie)
+
+//     movie.appendChild(movieTitle)
+//     let deleteBtn = document.createElement("button")
+//     deleteBtn.textContent = ('X')
+//     deleteBtn.addEventListener('click', deleteMovie)
+//     movie.appendChild(deleteBtn)
+//     document.querySelector('ul').appendChild(movie)
+//     document.querySelector('input').value = ""
+// }
+
+// const deleteMovie = (evt) => {
+//     evt.target.parentNode.remove()
+// }
+
+// const crossOffMovie = (evt) =>{
+//     evt.target.classList.toggle("checked")
+// }
+
+// document.querySelector('form').addEventListener('submit', addMovie)
+
+const addMovie = evt =>{
     evt.preventDefault()
-    let inputField = document.querySelector('input').value
-    let movie = document.createElement('li')
-    let movieTitle = document.createElement('span')
-
+    let inputField = document.querySelector("input").value
+    let movie = document.createElement("li")
+    let movieTitle = document.createElement("span")
 
     movieTitle.textContent = inputField
-    movieTitle.addEventListener('click', crossOffMovie)
-
+    
     movie.appendChild(movieTitle)
-    let deleteBtn = document.createElement("button")
-    deleteBtn.textContent = ('X')
-    deleteBtn.addEventListener('click', deleteMovie)
-    movie.appendChild(deleteBtn)
-    document.querySelector('ul').appendChild(movie)
-    document.querySelector('input').value = ""
-    message.textContent = "Movie Added!"
+
+    document.querySelector("ul").appendChild(movie)
 }
 
-const deleteMovie = (evt) => {
-    evt.target.parentNode.remove()
-    message.textContent = "Movie deleted!"
-}
-
-const crossOffMovie = (evt) =>{
-    evt.target.classList.toggle("checked")
-    message.textContent = "Movie checked!"
-}
-
-document.querySelector('form').addEventListener('submit', addMovie)
-
+document.querySelector("button").addEventListener("click", addMovie)
